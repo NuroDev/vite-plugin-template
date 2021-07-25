6ee19f19f9153f2c0b2bb26c9bd68fc1ebe7d4e0
nurodev <me@nuro.dev>
2021-07-25 20:57:00 +0100

    Added cross-env for production builds

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,15 @@
+import { env } from "process";
 import { resolve } from "path";
 
 import type { Options } from "tsup";
+
+const isProd = env.NODE_ENV === "production";
 
 export const tsup: Options = {
   clean: true,
   dts: true,
   entryPoints: [resolve(__dirname, "src", "index.ts")],
+  minify: isProd,
   sourcemap: true,
   splitting: false,
 };
